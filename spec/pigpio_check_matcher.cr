b1c41233f88aca2a70f357875d16a3fdf59eaf5b
Michael Wagner <mdwranger@gmail.com>
2021-07-29 23:36:46 -0400

    fix: custom matcher

--- spec/pigpio_check_matcher.cr
+++ spec/pigpio_check_matcher.cr
@@ -1,21 +1,28 @@
-struct PigpioCheckMatcher(ExpectedType) < Spectator::Matchers::ValueMatcher(ExpectedType)
-  def description : String
-    "is checked against x_pigpio#CHECK"
-  end
+module Spectator::Matchers
+  struct PigpioCheckMatcher(ExpectedType) < ValueMatcher(ExpectedType)
+    property pc : Int32 = 0
 
-  private def match?(actual : Spectator::TestExpression(T)) : Bool forall T
-    exp, pc = expected.value
-    ((actual.value >= (((1e2 - pc) * exp)/1e2)) && (actual.value <= (((1e2 + pc) * exp)/1e2)))
-  end
+    def with_pc(@pc)
+      self
+    end
 
-  private def failure_message(actual : Spectator::TestExpression(T)) : String forall T
-    "#{actual.label} failed to check against #{expected.label}"
+    def description : String
+      "is checked against x_pigpio#CHECK"
+    end
+
+    private def match?(actual : TestExpression(T)) : Bool forall T
+      exp = expected.value
+      ((actual.value >= (((1e2 - pc) * exp)/1e2)) && (actual.value <= (((1e2 + pc) * exp)/1e2)))
+    end
+
+    private def failure_message(actual : TestExpression(T)) : String forall T
+      "#{actual.label} failed to check against #{expected.label}"
+    end
   end
 end
 
 macro be_checked_against(expected, pc = 0)
-  %tuple = Tuple(Int32, Int32).new({{expected}}, {{pc}})
   %label = "{{expected}} (pc: {{pc}})"
-  %test_value = ::Spectator::TestValue(%tuple, %label)
-  PigpioCheckMatcher.new(%test_value)
+  %test_value = ::Spectator::TestValue.new({{expected}}, %label)
+  ::Spectator::Matchers::PigpioCheckMatcher.new(%test_value).with_pc({{pc}})
 end
