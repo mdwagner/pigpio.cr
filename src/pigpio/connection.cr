22305a9e2355338a1e3f6930b7413567ea821838
Michael Wagner <mdwranger@gmail.com>
2021-09-06 20:27:22 -0400

    feat: userland types

--- src/pigpio/connection.cr
+++ src/pigpio/connection.cr
@@ -1,13 +1,17 @@
 module Pigpio
+  alias OnAlertFn = LibPigpio::GpioAlertFuncT
+  alias OnEventFn = LibPigpio::EventFuncT
+  alias OnISRFn = LibPigpio::GpioISRFuncT
+
   class Connection
     @@alert_box : Void*?
 
-    def on_alert(pin, &block : LibPigpio::GpioAlertFuncT)
+    def on_alert(pin, &block : OnAlertFn)
       if block.closure?
         box = Box(typeof(block)).box(block)
         @@alert_box = box
 
-        f = GpioAlertFuncExT.new do |gpio, level, tick, userdata|
+        f = LibPigpio::GpioAlertFuncExT.new do |gpio, level, tick, userdata|
           Box(typeof(block)).unbox(userdata).call(gpio, level, tick)
         end
 
